fb401d54206bdf1cce6f93a1ed83997c39259b4a
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-08-03 16:28:55 +0100

    need to add whats your name?

---
+++ lib/battleships_web.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+
+class BattleshipsWeb < Sinatra::Base
+  get '/' do
+    'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  set :views, Proc.new { File.join(root, "..", "views") }
+  
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
